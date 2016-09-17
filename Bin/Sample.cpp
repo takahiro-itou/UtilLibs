e12ac4d01ebe214df82ad0ce4e2b2044d8fda0b1
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2016-09-17 17:35:08 +0900

    サンプルプログラム用のファイルを新規作成

---
+++ Bin/Sample.cpp
@@ -0,0 +1,24 @@
+//  -*-  coding: utf-8; mode: c++  -*-  //
+/*************************************************************************
+**                                                                      **
+**                      ---  Utility  Library  ---                      **
+**                                                                      **
+**          Copyright (C), 2001 - 2016, Takahiro Itou                   **
+**          All Rights Reserved.                                        **
+**                                                                      **
+*************************************************************************/
+
+/**
+**      サンプルプログラム。
+**
+**      @file       Sample.cpp
+**/
+
+#include    <UtilLibs/Common/UtilLibsSettings.h>
+
+#include    <iostream>
+
+int  main(int  argc,  char *  argv[])
+{
+    return ( 0 );
+}
