9727ac1d31a4c8ec5f7ea50f58015b13c898de0d
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2016-09-17 14:06:34 +0900

    プロジェクトの初期設定

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,2 +1,11 @@
 #! /bin/bash  -x
 
+mkdir  -p  .Config
+
+chmod  u+x  setuplinks.sh
+./setuplinks.sh
+
+aclocal  -I  .Config  \
+  &&  automake  --add-missing  --copy  --foreign  \
+  &&  autoconf
+
